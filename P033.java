fa258fded321956e39485c925b1105bd8d29ee6e
James Rothering <jr@jamesrothering.com>
2013-01-28 05:15:19 -0800

    a few more test pgms

---
+++ P033.java
@@ -0,0 +1,38 @@
+//Question: will this compile?
+class A
+{
+
+}
+
+class B
+{
+
+}
+
+class C{}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Answer: Yes,
+//Principle: a file can have many non-public (i.e., package-access) classes
---
+++ P034/A/P034.java
@@ -0,0 +1,36 @@
+//Will this compile?
+package A;
+class Test{}
+package A;
+class Another {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Answer: no.
+//Principle: Only one package statement is permissible in a source code file
+//           you cannot repeat even the same package declaration
